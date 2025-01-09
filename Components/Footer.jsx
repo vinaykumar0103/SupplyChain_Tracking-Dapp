0d75d0517ebd7c9a126f425136332bce3a63c77a
vinaykumar0103 <vinaykumar.borra01@gmail.com>
2025-01-09 22:32:26 +0530

    Footer updated

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Fot1, Fot2 } from "../Components/Footer";
+import { Fot1, Fot2 } from "../Components/index";
 
 export default () => {
   const footerNav = [
@@ -29,14 +29,14 @@ export default () => {
       <div className="sm:flex justify-between">
         {/* Left Section */}
         <div className="space-y-6">
-          <img src="https://www.floatui.com/logo.svg" alt="Logo" className="w-32" />
+          <img src="https://www.floatui.com/logo.svg"  className="w-32" />
           <p className="max-w-md">
             Nulla auctor metus vitae lectus iaculis, vel euismod massa efficitur.
           </p>
           <ul className="flex flex-wrap items-center gap-x-4 text-sm sm:text-base">
             {footerNav.map((item, idx) => (
-              <li key={idx} className="text-gray-800 hover:text-gray-500 duration-150">
-                <a href={item.href}>
+              <li  className="text-gray-800 hover:text-gray-500 duration-150">
+                <a key={idx} href={item.href}>
                   {item.name}
                 </a>
               </li>
@@ -48,10 +48,10 @@ export default () => {
         <div className="mt-6">
           <p className="text-gray-700 font-semibold">Get the app</p>
           <div className="flex items-center gap-3 mt-3 sm:block">
-            <a href="javascript:void(0)" aria-label="Download App 1">
+            <a href="javascript:void(0)">
               <Fot1 />
             </a>
-            <a href="javascript:void(0)" className="mt-0 block sm:mt-3" aria-label="Download App 2">
+            <a href="javascript:void(0)" className="mt-0 block sm:mt-3">
               <Fot2 />
             </a>
           </div>
