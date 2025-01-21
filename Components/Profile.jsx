349033cba6f31d999fb56a8f872ecc8e61210f2c
vinaykumar0103 <vinaykumar.borra01@gmail.com>
2025-01-21 22:00:33 +0530

    init commit

--- Components/Profile.jsx
+++ Components/Profile.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import React from 'react'
 
-const Profile = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Profile = async() => {
+    return(
+<div>
+    
+</div>
+    )
 }
-
-export default Profile
-
 
 export default Profile
